4cd33e44dd4814204a1f923b65293606da94df61
gursimran531 <prince.prince.ty4@gmail.com>
2025-09-18 12:01:04 +0000

    Added trim to fqdn for website

--- Terraform-3tier-Infra/modules/dns/outputs.tf
+++ Terraform-3tier-Infra/modules/dns/outputs.tf
@@ -1,4 +1,4 @@
 output "fqdomain" {
   description = "The domain name for the website"
-  value       = trim(aws_route53_record.root.fqdn)
+  value       = trim(tostring(aws_route53_record.root.fqdn))
 }
